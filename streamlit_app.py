62f9a2bf32d99bbc998eaab4f0720a92a922bfd4
Vimal Dewangan <vimal.dewangan.25@gmail.com>
2025-06-21 11:38:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,7 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered, '+name_on_order+'!',icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response
